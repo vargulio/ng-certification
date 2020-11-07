0def095555af948c7986909bcd6baac0542590e6
Slavozar Vargulev <slavozar.vargulev@gmail.com>
2020-11-07 22:03:00 +0200

    Created routing module and a main module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { MainModule } from "./main/main.module";
+import { AppRoutingModule } from "./app-routing.module";
+import { APP_BASE_HREF } from "@angular/common";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+    imports: [
+        BrowserModule,
+        FormsModule,
+        AppRoutingModule,
+        MainModule,
+    ],
+    declarations: [AppComponent, HelloComponent],
+    bootstrap: [AppComponent],
+    providers: [{provide: APP_BASE_HREF, useValue : '/' }]
 })
-export class AppModule { }
+export class AppModule {
+}
